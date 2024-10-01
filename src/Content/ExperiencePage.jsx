2241749110773c1a9a66c0f9bf4e66ebf63a1cbb
Adrian <adrianginergimenez@gmail.com>
2024-10-01 13:15:30 +0200

    feat: experience and links pages

--- src/Content/ExperiencePage.jsx
+++ src/Content/ExperiencePage.jsx
@@ -14,7 +14,7 @@ const ExperiencePage = () => {
         setChangePage(false);
         setCont(1)
 
-        if(changePage && cont > 0) {
+        if (changePage && cont > 0) {
             setChangePage(false);
             setExit(true);
         }
@@ -30,15 +30,178 @@ const ExperiencePage = () => {
             navigate(page);
         }, 400);
     };
-    
+
     return (
         <div className={!exit ? "experiencePage" : "experiencePage exit"}>
             <div className="experiencePage__space top"></div>
-            <div className="experiencePage__content">
-                <button className="experiencePage__button" onClick={() => handleClick("/skills")}>PREV</button>
-                <p>EXPERIENCE</p>
-                <button className="experiencePage__button" onClick={() => handleClick("/links")}>NEXT</button>
-            </div>
+            <article className="experiencePage__content">
+                <section className="experiencePage__time">
+                <p className="experiencePage__time--year">HOY</p>
+                    <p className="experiencePage__time--year">2018</p>
+                </section>
+                <section className="experiencePage__text">
+                    <ul className="experiencePage__list">
+                        <li className="experiencePage__listItem">
+                            <h2 className="experiencePage__list-tittle">
+                                Programador multidisciplinar
+                            </h2>
+                            <h3 className="experiencePage__list--subtittle">
+                                Freelance
+                            </h3>
+                            <p className="experiencePage__list--time">
+                                09/2023 - Actual
+                            </p>
+                            <p className="experiencePage__list--position">
+                                Remoto
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Desarrollo de videojuegos para plataformas android.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Desarrollo de servicios de Front y Back.
+
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Creación y gestión de bases de datos relacionales y no relacionales.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Despliegue de servicios.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Trabajo con repositorios de GitHub.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Documentación de código.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Testing manual.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Desarrollo de tests programaticos.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                <b>Visual Studio - Unity 3D - C# - JavaScript - HTML - CSS - SQL - MongoDB - GIT - React.js</b>
+                            </p>
+                        </li>
+                        <li className="experiencePage__listItem">
+                            <h2 className="experiencePage__list-tittle">
+                                Junior Programmer Unity 3D/C#
+                            </h2>
+                            <h3 className="experiencePage__list--subtittle">
+                                Digital Monster Collective
+                            </h3>
+                            <p className="experiencePage__list--time">
+                                06/2023 - 09/2023
+                            </p>
+                            <p className="experiencePage__list--position">
+                                Remoto
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Desarrollo de videojuegos 2D con Unity 3D.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Test e informes de los proyectos internos.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Análisis del código implementado.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Refactorización de código.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                <b>Visual Studio - Unity 3D - C# - Plastic SCM.</b>
+                            </p>
+                        </li>
+                        <li className="experiencePage__listItem">
+                            <h2 className="experiencePage__list-tittle">
+                                Gestión de expedientes en baja tensión
+                            </h2>
+                            <h3 className="experiencePage__list--subtittle">
+                                SEYP INGENIERIA SL
+                            </h3>
+                            <p className="experiencePage__list--time">
+                                06/2020 - 02/2022
+                            </p>
+                            <p className="experiencePage__list--position">
+                                Hibrido
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Analizar la viabilidad de nuevas peticiones de terceros en las redes de Distribución BT de Iberdrola.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Informar las condiciones técnico-económicas de las peticiones de terceros.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Coordinar los trabajos adjudicados con el contratista de Iberdrola.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Certificar y aprobar las mediciones del contratista de obra.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Inventariar en la cartografía interna de los sistemas de Iberdrola las nuevas
+                                instalaciones de peticiones aceptadas por terceros.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Cierre técnico-económico, con acta de puesta en marcha de obras aceptadas
+                                por terceros.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Registrar hoja instalaciones de enlace en las peticiones aceptadas por
+                                terceros.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                <b>Suite Ofimática Windows Office, SAP, herramientas internas.</b>
+                            </p>
+                        </li>
+                        <li className="experiencePage__listItem">
+                            <h2 className="experiencePage__list-tittle">
+                                Gestor de expedientes electricos
+                            </h2>
+                            <h3 className="experiencePage__list--subtittle">
+                                IDEA INGENIERÍA
+                            </h3>
+                            <p className="experiencePage__list--time">
+                                10/2018 - 02/2020
+                            </p>
+                            <p className="experiencePage__list--position">
+                                Presencial
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Analizar la viabilidad de nuevas peticiones de terceros en las redes de Distribución BT de Iberdrola.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Informar las condiciones técnico-económicas de las peticiones de terceros.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Coordinar los trabajos adjudicados con el contratista de Iberdrola.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Certificar y aprobar las mediciones del contratista de obra.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Inventariar en la cartografía interna de los sistemas de Iberdrola las nuevas
+                                instalaciones de peticiones aceptadas por terceros.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Cierre técnico-económico, con acta de puesta en marcha de obras aceptadas
+                                por terceros.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                - Registrar hoja instalaciones de enlace en las peticiones aceptadas por
+                                terceros.
+                            </p>
+                            <p className="experiencePage__list--content">
+                                <b>Suite Ofimática Windows Office, SAP, herramientas internas.</b>
+                            </p>
+                        </li>
+                    </ul>
+                </section>
+                <section className="experiencePage__buttonWrap">
+                    <button className="experiencePage__button" onClick={() => handleClick("/skills")}>PREV</button>
+                    <p>Una coleccion de experiencias profesionales</p>
+                    <button className="experiencePage__button" onClick={() => handleClick("/links")}>NEXT</button>
+                </section>
+            </article>
             <div className="experiencePage__space bottom"></div>
         </div>)
 
--- src/Content/LinksPage.jsx
+++ src/Content/LinksPage.jsx
@@ -1,7 +1,12 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useChangePageContext } from "../Context/useChangePageContext";
 import { useEffect, useState } from "react";
 import './LinksPage.css'
+
+import LinkedinImg from "../assets/LinksImg/Linkedin.jpg";
+import GithubImg from "../assets/LinksImg/Github.jpg";
+import WildcattaImg from "../assets/LinksImg/WildCatta.png";
+import GamerrestImg from "../assets/LinksImg/GamerRest.jpg";
 
 const LinksPage = () => {
     const { changePage, setChangePage } = useChangePageContext();
@@ -14,7 +19,7 @@ const LinksPage = () => {
         setChangePage(false);
         setCont(1)
 
-        if(changePage && cont > 0) {
+        if (changePage && cont > 0) {
             setChangePage(false);
             setExit(true);
         }
@@ -30,15 +35,50 @@ const LinksPage = () => {
             navigate(page);
         }, 400);
     };
-    
+
     return (
         <div className={!exit ? "linksPage" : "linksPage exit"}>
             <div className="linksPage__space top"></div>
-            <div className="linksPage__content">
-                <button className="linksPage__button" onClick={() => handleClick("/experience")}>PREV</button>
-                <p>LINKS</p>
-                <button className="linksPage__button" onClick={() => handleClick("/contact")}>NEXT</button>
-            </div>
+            <article className="linksPage__content">
+                <section className="linksPage__linksWrap">
+                    <ul className="linksPage__list">
+                        <Link to="https://www.linkedin.com/in/aginergimenez/" className="linksPage__link" target="_blank" rel="noopener noreferrer">
+                            <li className="linksPage__listItem">
+                                <h1 className="linksPage__linkText">LinkedIn</h1>
+                                <h2 className="linksPage__linkSubtext">Mi red social para el sector profesional</h2>
+                                <img className="linksPage__linkImg" src={LinkedinImg} alt="linkedin image" />
+                            </li>
+                        </Link>
+                        <Link to="https://github.com/AGGenius" className="linksPage__link" target="_blank" rel="noopener noreferrer">
+                            <li className="linksPage__listItem">
+                                <h1 className="linksPage__linkText">Github</h1>
+                                <h2 className="linksPage__linkSubtext">Todos mis repositorios para que puedas echar un ojo a mis skills de programación</h2>
+                                <img className="linksPage__linkImg" src={GithubImg} alt="linkedin image" />
+                            </li>
+                        </Link>
+                        <Link to="https://play.google.com/store/apps/developer?id=Wild+Catta&hl=es" className="linksPage__link" target="_blank" rel="noopener noreferrer">
+                            <li className="linksPage__listItem">
+                                <h1 className="linksPage__linkText">Google Play</h1>
+                                <h2 className="linksPage__linkSubtext">Mi perfil de desarrollador para Android</h2>
+                                <img className="linksPage__linkImg" src={WildcattaImg} alt="linkedin image" />
+                            </li>
+                        </Link>
+                        <Link to="https://gamefeed-front.onrender.com/" className="linksPage__link" target="_blank" rel="noopener noreferrer">
+                            <li className="linksPage__listItem">
+                                <h1 className="linksPage__linkText">Gamer Rest</h1>
+                                <h2 className="linksPage__linkSubtext">Mi web y proyecto personal como Full Stack Web Developer</h2>
+                                <img className="linksPage__linkImg" src={GamerrestImg} alt="linkedin image" />
+                            </li>
+
+                        </Link>
+                    </ul>
+                </section>
+                <section className="linksPage__buttonWrap">
+                    <button className="linksPage__button" onClick={() => handleClick("/experience")}>PREV</button>
+                    <p>¡No todo puede contenerse en una unica web!</p>
+                    <button className="linksPage__button" onClick={() => handleClick("/contact")}>NEXT</button>
+                </section>
+            </article>
             <div className="linksPage__space bottom"></div>
         </div>)
 
